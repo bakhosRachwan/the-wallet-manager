0d9acfbe8d8f65ce1e3140bdc443cdde0bc0c841
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-13 23:58:28 +0300

    Refactor: update hosting

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,103 +1,162 @@
-import { Box, Text, Input, Stack, Button, Radio, RadioGroup } from "@chakra-ui/react"
+import {
+  Box,
+  Text,
+  Input,
+  Stack,
+  Button,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router";
 import { StateContext } from "../State";
 
 const TransactionForm = () => {
-    const [state, dispatch] = useContext(StateContext);
-    const {register, handleSubmit} = useForm();
-    const { name } = useParams();
-        
-    const Onsubmit = (data, e) => {
-        let transaction = {
-            ...data,
-            walletname: name,
-            date: new Date().toDateString()
-        }
-        console.log(state)
-        dispatch({ type: "SET_TRANSACTIONS", payload: transaction });
-        e.target.reset();
-      };
-    return (
-        <Box w="100%" display={{md:"flex"}} justifyContent="center" >
-            <form onSubmit={handleSubmit(Onsubmit)}>
-                <Stack spacing="4">
-                    <Box w={[100, 100, 500, 1500]} display={{lg: "flex"}} justifyContent={{md: "space-evenly"}}>
-                        <Box  display={{md:"flex"}} w={[250, 100, 500, 1200]} justifyContent="space-between" alignItems="center">
-                            <Text>Transaction:</Text>
-                            <Input  type="number" min={0} w="75%" variant="filled" placeholder="Enter Transaction" name="transaction" ref={register} required  />
-                        </Box>
-                        <Box >
-                            <RadioGroup display={{md:"flex"}} w={[220, 100, 300]} justifyContent="space-evenly" required >
-                        <Radio 
-                            value="Income"
-                            ref={register}
-                            name="tranType"
-                            cursor="pointer"
-                            type="radio"
-                            borderWidth="1px"
-                            borderRadius="md"
-                            boxShadow="md"
-                            _checked={{
-                                bg: "teal.600",
-                                color: "red",
-                                borderColor: "teal.600",
-                            }}
-                            _focus={{
-                                boxShadow: "outline",
-                            }}
-                            px={5}
-                            py={3}
-                        >
-                            Income
-                        </Radio >
-                        <Radio 
-                            value="Expense"
-                            name="tranType"
-                            ref={register}
-                            cursor="pointer"
-                            borderWidth="1px"
-                            borderRadius="md"
-                            boxShadow="md"
-                            _checked={{
-                                bg: "red.600",
-                                color: "red",
-                                borderColor: "red.600",
-                            }}
-                            _focus={{
-                                boxShadow: "outline",
-                            }}
-                            px={5}
-                            py={3}
-                        >
-                            Expense
-                        </Radio >
-                        </RadioGroup>
-                        </Box>
-                        <Box  display={{md: "flex"}} justifyContent="space-between" alignItems={{md: "center"}} w={[220, 100, 500, 1200]} px={{md: "4"}}>
-                            <Text>Notes:</Text>
-                            <Input type="text" w="100%" variant="filled" placeholder="Enter Notes" name="notes" ref={register} required  />
-                        </Box>
-                        <Box display={{md: "flex"}} w={[220, 100, 500, 1200]} justifyContent="space-between" alignItems={{md:"center"}}>
-                            <Text>Tags:</Text>
-                            <Input type="text" w="100%" variant="filled" placeholder="Enter Tags Seperated by COMMAS" name="tags" ref={register} required  />
-                        </Box>
-                    </Box>
-                    <Button 
-                        type="submit" 
-                        w={{md:"25%"}} 
-                        alignSelf="center" 
-                        _hover={{
-                            bg: "green.600", 
-                            boxShadow: "outline",
-                        }}
-                        >Submit
-                    </Button>
-                </Stack>
-            </form>
-        </Box>
-    );
-}
+  const [state, dispatch] = useContext(StateContext);
+  const { register, handleSubmit } = useForm();
+  const { name } = useParams();
+
+  const Onsubmit = (data, e) => {
+    let transaction = {
+      ...data,
+      walletname: name,
+      date: new Date().toDateString(),
+    };
+    console.log(state);
+    dispatch({ type: "SET_TRANSACTIONS", payload: transaction });
+    e.target.reset();
+  };
+  return (
+    <Box w="100%" display={{ md: "flex" }} justifyContent="center">
+      <form onSubmit={handleSubmit(Onsubmit)}>
+        <Stack spacing="4">
+          <Box
+            w={[100, 100, 500, 1500]}
+            display={{ lg: "flex" }}
+            justifyContent={{ md: "space-evenly" }}
+          >
+            <Box
+              display={{ md: "flex" }}
+              w={[250, 100, 500, 1200]}
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text>Transaction:</Text>
+              <Input
+                type="number"
+                min={0}
+                w="75%"
+                variant="filled"
+                placeholder="Enter Transaction"
+                name="transaction"
+                ref={register}
+                required
+              />
+            </Box>
+            <Box>
+              <RadioGroup
+                display={{ md: "flex" }}
+                w={[220, 100, 300]}
+                justifyContent="space-evenly"
+                required
+              >
+                <Radio
+                  value="Income"
+                  ref={register}
+                  name="tranType"
+                  cursor="pointer"
+                  type="radio"
+                  borderWidth="1px"
+                  borderRadius="md"
+                  boxShadow="md"
+                  _checked={{
+                    bg: "teal.600",
+                    color: "red",
+                    borderColor: "teal.600",
+                  }}
+                  _focus={{
+                    boxShadow: "outline",
+                  }}
+                  px={5}
+                  py={3}
+                >
+                  Income
+                </Radio>
+                <Radio
+                  value="Expense"
+                  name="tranType"
+                  ref={register}
+                  cursor="pointer"
+                  borderWidth="1px"
+                  borderRadius="md"
+                  boxShadow="md"
+                  _checked={{
+                    bg: "red.600",
+                    color: "red",
+                    borderColor: "red.600",
+                  }}
+                  _focus={{
+                    boxShadow: "outline",
+                  }}
+                  px={5}
+                  py={3}
+                >
+                  Expense
+                </Radio>
+              </RadioGroup>
+            </Box>
+            <Box
+              display={{ md: "flex" }}
+              justifyContent="space-between"
+              alignItems={{ md: "center" }}
+              w={[220, 100, 500, 1200]}
+              px={{ md: "4" }}
+            >
+              <Text>Notes:</Text>
+              <Input
+                type="text"
+                w="100%"
+                variant="filled"
+                placeholder="Enter Notes"
+                name="notes"
+                ref={register}
+                required
+              />
+            </Box>
+            <Box
+              display={{ md: "flex" }}
+              w={[220, 100, 500, 1200]}
+              justifyContent="space-between"
+              alignItems={{ md: "center" }}
+            >
+              <Text>Tags:</Text>
+              <Input
+                type="text"
+                w="100%"
+                variant="filled"
+                placeholder="Enter Tags Seperated by COMMAS"
+                name="tags"
+                ref={register}
+                required
+              />
+            </Box>
+          </Box>
+          <Button
+            type="submit"
+            w={{ md: "25%" }}
+            alignSelf="center"
+            _hover={{
+              bg: "green.600",
+              boxShadow: "outline",
+            }}
+          >
+            Submit
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
 
 export default TransactionForm;
--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,4 +1,4 @@
-import { Input, Stack, Text, Button, Box, Select } from "@chakra-ui/react"
+import { Input, Stack, Text, Button, Box, Select } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import { StateContext } from "../State";
 import { useForm } from "react-hook-form";
@@ -7,59 +7,93 @@ import { useHistory } from "react-router";
 // import { uuid } from "uuidv4";
 
 const WalletForm = () => {
-    const [state, dispatch] = useContext(StateContext);
-    const {register, handleSubmit} = useForm();
-    const history = useHistory();
+  const [state, dispatch] = useContext(StateContext);
+  const { register, handleSubmit } = useForm();
+  const history = useHistory();
 
   const handleCreateWallet = (data, e) => {
-      let wallet = data
+    let wallet = data;
     console.log(state);
     console.log(wallet);
     dispatch({ type: "SET_WALLETS", payload: wallet });
-    history.push(`/transaction/${data.name}`)
+    history.push(`/transaction/${data.name}`);
     e.target.reset();
   };
 
-    return (
-        <Box  h="90%" p="2" display="flex" alignItems="center" justifyContent="center">
-            <form onSubmit={handleSubmit(handleCreateWallet)} style={{width: "50%"}}>
-                <Stack spacing={2}>
-                    <Box display="flex" justifyContent="space-between">
-                        <Text>Name:</Text>
-                        <Input type="name" name="name" size="md" w="75%" variant="filled" placeholder="Enter Name" ref={register} required  />
-                    </Box>
-                    <Box display="flex" justifyContent="space-between">
-                        <Text>Currency:</Text>
-                        <Select w="75%" name="currency" variant="filled" ref={register}>
-                            <option>Dollar</option>
-                            <option>Euro</option>
-                            <option>Lebanese Pound</option>
-                        </Select>
-                    </Box>
-                    <Box display="flex" justifyContent="space-between">
-                        <Text>Balance:</Text>
-                        <Input type="number" min={0} w="75%" variant="filled" placeholder="Enter Initial Balance" name="balance" ref={register} required  />
-                    </Box>
-                    <Box display="flex" justifyContent="space-between">
-                        <Text>Description:</Text>
-                        <Input type="text" w="75%" variant="filled" placeholder="Wallet Description" name="desc" ref={register} required  />
-                    </Box>
-                    <Button 
-                        type="submit" 
-                        w="50%" 
-                        alignSelf="center" 
-                        colorScheme="blue"
-                        _hover={{
-                            // bgGradient: "radial( #ff5f6d, #ffc371)",
-                            // bg: "green.600", 
-                            boxShadow: "outline",
-                        }}
-                        >Create Wallet
-                    </Button>
-                </Stack>
-            </form>
-        </Box>
-    );
-}
+  return (
+    <Box
+      h="90%"
+      p="2"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <form
+        onSubmit={handleSubmit(handleCreateWallet)}
+        style={{ width: "50%" }}
+      >
+        <Stack spacing={2}>
+          <Box display={{ md:"flex"}} justifyContent="space-between">
+            <Text>Name:</Text>
+            <Input
+              type="name"
+              name="name"
+              size="md"
+              w="75%"
+              variant="filled"
+              placeholder="Enter Name"
+              ref={register}
+              required
+            />
+          </Box>
+          <Box display={{ md:"flex"}} justifyContent="space-between">
+            <Text>Currency:</Text>
+            <Select w="75%" name="currency" variant="filled" ref={register}>
+              <option>Dollar</option>
+              <option>Euro</option>
+              <option>Lebanese Pound</option>
+            </Select>
+          </Box>
+          <Box display={{ md:"flex"}} justifyContent="space-between">
+            <Text>Balance:</Text>
+            <Input
+              type="number"
+              min={0}
+              w="75%"
+              variant="filled"
+              placeholder="Enter Initial Balance"
+              name="balance"
+              ref={register}
+              required
+            />
+          </Box>
+          <Box display={{ md:"flex"}} justifyContent="space-between">
+            <Text>Description:</Text>
+            <Input
+              type="text"
+              w="75%"
+              variant="filled"
+              placeholder="Wallet Description"
+              name="desc"
+              ref={register}
+              required
+            />
+          </Box>
+          <Button
+            type="submit"
+            w={[ "75%", "50%"]}
+            alignSelf={{md:"center"}}
+            colorScheme="blue"
+            _hover={{
+              boxShadow: "outline",
+            }}
+          >
+            Create Wallet
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
 
 export default WalletForm;
